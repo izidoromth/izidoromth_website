ff607ccad403e09b4e052692f1509aa07943512f
Matheus Izidoro de Almeida <izidoro@radek.com.br>
2022-03-21 22:13:39 -0300

    website developed

---
+++ constants/data.js
@@ -0,0 +1,60 @@
+const userData = {
+  githubUsername: "izidoromth",
+  name: "Matheus Izidoro",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "matheusizidoro07@gmail.com",
+  phone: "+55 41 99885 2896",
+  address: "Curitiba, Paraná, Brasil.",
+  projects: [
+    {
+      title: "",
+      link: "",
+      imgUrl: "",
+    }
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves to solve problems and create beautiful and easy to use applications. Currently working on Radek Information Systems",
+    description: [
+      `I'm in the world of technology since I was a kid when my father bought our very first computer. I was introduced to programming at 12 when I used Blender to create a game even that at that moment I didn't know that it was called programming.`,
+      `At 14 I started my technology high school entering in Computer Engineering graduation after that. Since then I've been working on a vast variety of projects from games, mobile and desktop applications to robots!`,
+      `I'm currently studying and improving my data science skills for my graduation thesis.`,
+    ],
+    currentProject: "",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Graduation",
+      company: "Federal University of Technology - Paraná, UTFPR.",
+      year: "2022",
+      companyLink: "https://portal.utfpr.edu.br",
+      desc: "Computer Engineer 💻",
+    },
+    {
+      title: "Software Developer",
+      company: "Radek Information Systems",
+      year: "2019",
+      companyLink: "https://radek.com.br",
+      desc: "I've been working with all kinds of software, from mobile apps to front/back-end and desktop applications 💻🖱️",
+    },
+    {
+      title: "High School of Technology",
+      company: "Paraná Federal Institute of Education, Science and Technology, IFPR",
+      year: "2015",
+      companyLink: "https://reitoria.ifpr.edu.br",
+      desc: "Electronics Technician 🔌",
+    },
+  ],
+  resumeUrl:
+    "",
+  socialLinks: {
+    instagram: "https://instagram.com/izidoro.mth",
+    linkedin: "https://www.linkedin.com/in/matheus-izidoro-de-almeida-463533123/",
+    github: "https://github.com/izidoromth",
+    facebook: "https://www.facebook.com/matheusizidorodealmeida",
+  },
+};
+
+export default userData;
